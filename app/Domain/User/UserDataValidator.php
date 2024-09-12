0236108d2f6e2db37f775eeb4e8c5f4cad63985e
Cleifson <cl.raujo1@gmail.com>
2024-09-12 15:39:16 -0300

    fix(user): improve data validation methods

--- app/Domain/User/UserDataValidator.php
+++ app/Domain/User/UserDataValidator.php
@@ -18,34 +18,82 @@ class UserDataValidator implements UserDataValidatorInterface
         /*
             TODO: validate id using UUID v4 pattern
         */
+        if (!preg_match(self::UUID_REGEX, $id)) {
+            throw new DataValidationException('The user id is not valid');
+        }
     }
 
     public function validateName(string $name): void
     {
-        
+        if (empty($name)) {
+            throw new DataValidationException('The user name cannot be empty');
+        }
+
+        if (strlen($name) > self::NAME_MAX_LEGTH) {
+            throw new DataValidationException('The user name exceeds the max length');
+        }
     }
 
     public function validateEmail(string $email): void
     {
-        
+        if (empty($email)) {
+            throw new DataValidationException('The user email cannot be empty');
+        }
+
+        if (strlen($email) > self::EMAIL_MAX_LEGTH) {
+            throw new DataValidationException('The user email exceeds the max length');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new DataValidationException('The user email is not valid');
+        }
     }
 
     public function validateCpf(string $cpf): void
     {
         $trimmedCpf = trim($cpf);
 
-        if (!(new Cpf($trimmedCpf))->isValid()) {
+        if (empty($trimmedCpf)) {
+            throw new DataValidationException('The user cpf cannot be empty');
+        }
+
+        if (preg_match('/[^\d]/', $trimmedCpf)) {
+            throw new DataValidationException('The user cpf is not valid');
+        }
+
+        $formattedCpf = preg_replace('/\D/', '', $trimmedCpf);
+
+        if (strlen($formattedCpf) !== 11 || !is_numeric($formattedCpf) || !(new Cpf($formattedCpf))->isValid()) {
             throw new DataValidationException('The user cpf is not valid');
         }
     }
 
+
     public function validateDateCreation(string $dateCreation): void
     {
-        
+        if (empty($dateCreation)) {
+            throw new DataValidationException('The user date creation cannot be empty');
+        }
+
+        if (!$this->isValidDate($dateCreation)) {
+            throw new DataValidationException('The user date creation is not in a valid format');
+        }
     }
 
     public function validateDateEdition(string $dateEdition): void
     {
-       
+        if (empty($dateEdition)) {
+            throw new DataValidationException('The user date edition cannot be empty');
+        }
+
+        if (!$this->isValidDate($dateEdition)) {
+            throw new DataValidationException('The user date edition is not in a valid format');
+        }
+    }
+
+    private function isValidDate(string $date): bool
+    {
+        $dateTime = \DateTime::createFromFormat('Y-m-d H:i:s', $date);
+        return $dateTime && $dateTime->format('Y-m-d H:i:s') === $date;
     }
 }
