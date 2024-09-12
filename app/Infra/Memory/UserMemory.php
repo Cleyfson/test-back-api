3a4d171efcfa51931e18225a108b0bf8d654c0d4
Cleifson <cl.raujo1@gmail.com>
2024-09-12 15:47:16 -0300

    test(mock): enhance user-memory for correct testing

--- app/Infra/Memory/UserMemory.php
+++ app/Infra/Memory/UserMemory.php
@@ -7,43 +7,61 @@ use App\Domain\User\UserPersistenceInterface;
 
 class UserMemory implements UserPersistenceInterface
 {
+    private array $users = [];
+
     public function create(User $user): void
     {
-
+        $this->users[$user->getId()] = $user;
     }
 
     public function isCpfAlreadyCreated(User $user): bool
     {
+        foreach ($this->users as $existingUser) {
+            if ($existingUser->getCpf() === $user->getCpf()) {
+                return true;
+            }
+        }
         return false;
     }
 
     public function isEmailAlreadyCreated(User $user): bool
     {
+        foreach ($this->users as $existingUser) {
+            if ($existingUser->getEmail() === $user->getEmail()) {
+                return true;
+            }
+        }
         return false;
     }
 
     public function findAll(User $user): array
     {
-        return [];
+        return array_values($this->users);
     }
 
     public function isExistentId(User $user): bool
     {
-        return true;
+        return isset($this->users[$user->getId()]);
     }
 
     public function editName(User $user): void
     {
-
+        if (isset($this->users[$user->getId()])) {
+            $this->users[$user->getId()]->setName($user->getName());
+        }
     }
 
     public function editCpf(User $user): void
     {
-
+        if (isset($this->users[$user->getId()])) {
+            $this->users[$user->getId()]->setCpf($user->getCpf());
+        }
     }
 
     public function editEmail(User $user): void
     {
-
+        if (isset($this->users[$user->getId()])) {
+            $this->users[$user->getId()]->setEmail($user->getEmail());
+        }
     }
 }
